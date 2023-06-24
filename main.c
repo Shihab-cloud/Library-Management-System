eba306ec10edb0ea6f7d14bccca335ad4c770e76
Shihab-cloud <134206316+Shihab-cloud@users.noreply.github.com>
2023-06-24 23:41:42 +0600

    Update main.c
    adding features and log in menu

--- main.c
+++ main.c
@@ -102,3 +102,35 @@ sleep(5);
 system("cls");
 
 }
+
+void login_h(void){
+
+    char username[15];
+    char password[12];
+
+
+    printf("Enter your username:\n");
+    scanf("%s",&username);
+
+    printf("Enter your password:\n");
+    scanf("%s",&password);
+
+    if(strcmp(username,"Shihab")==0){
+        if(strcmp(password,"123")==0){
+
+        printf("\nWelcome.Login Success!");
+
+
+        }else{
+    printf("\nwrong password");
+    sleep(5000);
+             }
+    }else{
+    printf("\nUser doesn't exist");
+    sleep(5000);
+         }
+     sleep(3);
+
+     system("cls");
+}
+

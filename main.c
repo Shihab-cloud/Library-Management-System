da9a4f70ec3075896ea8d941dbfc9f6ceba6322a
Shihab-cloud <134206316+Shihab-cloud@users.noreply.github.com>
2023-06-25 00:48:38 +0600

    Other functions
    adding other functions

--- main.c
+++ main.c
@@ -134,3 +134,146 @@ void login_h(void){
      system("cls");
 }
 
+void addBook(void){
+    char myDate[12];
+    time_t t = time(NULL);
+    struct tm tm = *localtime(&t);
+    sprintf(myDate, "%02d/%02d/%d", tm.tm_mday, tm.tm_mon+1, tm.tm_year + 1900);
+    strcpy(b.date, myDate);
+
+    fp = fopen("books.txt", "ab");
+
+    printf("Enter book id: ");
+    scanf("%d", &b.id);
+
+    printf("Enter book name: ");
+    fflush(stdin);
+    gets(b.bookName);
+
+    printf("Enter author name: ");
+    fflush(stdin);
+    gets(b.authorName);
+
+    printf("Book Added Successfully");
+
+    fwrite(&b, sizeof(b), 1, fp);
+    fclose(fp);
+}
+
+
+void booksList(void){
+
+    system("cls");
+    printf("<== Available Books ==>\n\n");
+    printf("%-10s %-30s %-20s %s\n\n", "Book id", "Book Name", "Author", "Date");
+
+    fp = fopen("books.txt", "rb");
+    while(fread(&b, sizeof(b), 1, fp) == 1){
+        printf("%-10d %-30s %-20s %s\n", b.id, b.bookName, b.authorName, b.date);
+    }
+
+    fclose(fp);
+}
+
+void del(void){
+    int id, f=0;
+    system("cls");
+    printf("<== Remove Books ==>\n\n");
+    printf("Enter Book id to remove: ");
+    scanf("%d", &id);
+
+    FILE *ft;
+
+    fp = fopen("books.txt", "rb");
+    ft = fopen("temp.txt", "wb");
+
+    while(fread(&b, sizeof(b), 1, fp) == 1){
+        if(id == b.id){
+            f=1;
+        }else{
+            fwrite(&b, sizeof(b), 1, ft);
+        }
+    }
+
+    if(f==1){
+        printf("\n\nDeleted Successfully.");
+    }else{
+        printf("\n\nRecord Not Found !");
+    }
+
+    fclose(fp);
+    fclose(ft);
+
+    remove("books.txt");
+    rename("temp.txt", "books.txt");
+
+}
+
+
+void issueBook(void){
+
+    char myDate[12];
+    time_t t = time(NULL);
+    struct tm tm = *localtime(&t);
+    sprintf(myDate, "%02d/%02d/%d", tm.tm_mday, tm.tm_mon+1, tm.tm_year + 1900);
+    strcpy(s.date, myDate);
+
+    int f=0;
+
+    system("cls");
+    printf("<== Issue Books ==>\n\n");
+
+    printf("Enter Book id to issue: ");
+    scanf("%d", &s.id);
+
+
+    fp = fopen("books.txt", "rb");
+
+    while(fread(&b, sizeof(b), 1, fp) == 1){
+        if(b.id == s.id){
+            strcpy(s.bookName, b.bookName);
+            f=1;
+            break;
+        }
+    }
+
+    if(f==0){
+        printf("No book found with this id\n");
+        printf("Please try again...\n\n");
+        return;
+    }
+
+    fp = fopen("issue.txt", "ab");
+
+    printf("Enter Student Name: ");
+    fflush(stdin);
+    gets(s.sName);
+
+    printf("Enter Student Class: ");
+    fflush(stdin);
+    gets(s.sClass);
+
+    printf("Enter Student Roll: ");
+    scanf("%d", &s.sRoll);
+
+    printf("Book Issued Successfully\n\n");
+
+    fwrite(&s, sizeof(s), 1, fp);
+    fclose(fp);
+}
+
+void issueList(void){
+    system("cls");
+    printf("<== Book Issue List ==>\n\n");
+
+    printf("%-10s %-30s %-20s %-10s %-30s %s\n\n", "S.id", "Name", "Class", "Roll", "Book Name", "Date");
+
+    fp = fopen("issue.txt", "rb");
+    while(fread(&s, sizeof(s), 1, fp) == 1){
+        printf("%-10d %-30s %-20s %-10d %-30s %s\n", s.id, s.sName, s.sClass, s.sRoll, s.bookName, s.date);
+    }
+
+    fclose(fp);
+}
+
+
